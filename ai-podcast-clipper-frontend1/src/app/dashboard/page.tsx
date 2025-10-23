27aa5440c866e598597d69c01e1be92e880ed7f8
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-10-24 00:10:24 +0530

    created dashboard page

--- ai-podcast-clipper-frontend1/src/app/dashboard/page.tsx
+++ ai-podcast-clipper-frontend1/src/app/dashboard/page.tsx
@@ -1,4 +1,55 @@
-"use server"
+"use server";
+
+import { redirect } from "next/navigation";
+import { DashboardClient } from "~/components/ui/dashboard-client";
+import { auth } from "~/server/auth";
+import { db } from "~/server/db";
+
 export default async function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/login");
+  }
+
+  const userData = await db.user.findUniqueOrThrow({
+    where: { id: session.user.id },
+    select: {
+      UploadedFile: {
+        where: {
+          uploaded: true,
+        },
+        select: {
+          id: true,
+          s3Key: true,
+          displayName: true,
+          status: true,
+          createdAt: true,
+          _count: {
+            select: {
+              clips: true,
+            },
+          },
+        },
+      },
+      Clip: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+
+  const formattedFiles = userData.UploadedFile.map((file) => ({
+    id: file.id,
+    s3Key: file.s3Key,
+    filename: file.displayName ?? "Unknown filename",
+    status: file.status,
+    clipsCount: file._count.clips,
+    createdAt: file.createdAt,
+  }));
+
+  return (
+    <DashboardClient uploadedFiles={formattedFiles} clips={userData.Clip} />
+  );
 }
